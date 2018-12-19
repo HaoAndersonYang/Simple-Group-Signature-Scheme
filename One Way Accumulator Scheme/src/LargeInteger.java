9636a4ebe23200a26c3135a5d98680f328bc8298
Hao Yang <yanghao.anderson@gmail.com>
2018-12-19 02:02:44 -0500

    Update of implementation of large integer class
    Changed implementation to circular array.
    Implement Karatsuba Multiplication for largeinteger class

--- One Way Accumulator Scheme/src/LargeInteger.java
+++ One Way Accumulator Scheme/src/LargeInteger.java
@@ -3,44 +3,90 @@ import java.util.LinkedList;
 // Assume no leading zero
 public class LargeInteger implements Comparable<LargeInteger> {
 
-    public LinkedList<Byte> value;
+    public Byte[] value = new Byte[1000];
+    public int head = 0;//value[head] is the head
+    public int tail = 0;//value[tail] is place to insert
+    public int size = 0;
 
-    public LargeInteger(LinkedList<Byte> value) {
+    public LargeInteger(Byte[] value) {
         this.value = value;
     }
 
     public LargeInteger(LargeInteger li) {
-        this.value = new LinkedList<>(li.value);
+        this.value = li.value.clone();
     }
 
     public LargeInteger(String input) {
-        value = new LinkedList<>();
         for (int i = 0; i < input.length(); i++) {
             if (input.charAt(i) == '1') {
-                value.offerLast((byte) 1);
+                this.insertAtLast((byte) 1);
             }
             if (input.charAt(i) == '0') {
-                value.offerLast((byte) 0);
+                this.insertAtLast((byte) 0);
             }
         }
     }
 
+    //Single bit
+    public LargeInteger(int i) {
+        this.insertAtHead((byte) i);
+    }
+
+    private void insertAtLast(byte insert) {
+        value[tail] = insert;
+        tail += 1;
+        size += 1;
+        if (tail >= value.length) {
+            tail = 0;
+        }
+        if (size > value.length) {
+            System.out.println("ERROR: OVERFLOW");
+            System.exit(-1);
+        }
+    }
+
+    public byte removeLast() {
+        byte res = value[tail - 1];
+        tail -= 1;
+        size -= 1;
+        if (tail < 0) {
+            tail = value.length - 1;
+        }
+        if (size <= 0) {
+            System.out.println("ERROR: INVALID REMOVE");
+            System.exit(-1);
+        }
+        return res;
+    }
+
+    private void insertAtHead(byte insert) {
+        if (head == 0) {
+            head = value.length;
+        }
+        head -= 1;
+        value[head] = insert;
+        size += 1;
+        if (size > value.length) {
+            System.out.println("ERROR: OVERFLOW");
+            System.exit(-1);
+        }
+    }
+
     public LargeInteger() {
-        value = new LinkedList<>();
-        value.offer((byte) 0);
+        this.insertAtHead((byte) 0);
     }
 
     //https://en.wikipedia.org/wiki/Modular_exponentiation#Right-to-left_binary_method
     public LargeInteger modular_pow(LargeInteger exp, LargeInteger mod) {
         LargeInteger exponent = new LargeInteger(exp);
         LargeInteger module = new LargeInteger(mod);
-        if (module.value.size() == 1) { //Either mod 1 or mod 0.
+        if (module.size == 1) { //Either mod 1 or mod 0.
             return new LargeInteger();
         }
         LargeInteger base = this.mod(module);
         LargeInteger result = new LargeInteger("1");
         while (exponent.compareTo(new LargeInteger()) > 0) {
-            if (exponent.value.getLast() == 1) {
+            if (exponent.value[exponent.tail - 1] != 0) {
                 result = (result.multiply(base)).mod(module);
             }
             exponent = exponent.shiftRight();
@@ -50,68 +96,72 @@ public class LargeInteger implements Comparable<LargeInteger> {
     }
 
     public LargeInteger shiftRight() {
-        LinkedList<Byte> newList = new LinkedList<>(value);
-        newList.removeLast();
-        return new LargeInteger(newList);
+        if (this.size == 1) {
+            System.out.println("ERROR: Invalid shift");
+            System.exit(-1);
+        }
+        LargeInteger result = new LargeInteger(this);
+        result.tail -= 1;
+        result.size -= 1;
+        return result;
     }
 
     //https://en.wikipedia.org/wiki/Multiplication_algorithm#Peasant_or_binary_multiplication
+    //Assuming both have same number of bits
     public LargeInteger multiply(LargeInteger multiplier) {
-        LargeInteger result = new LargeInteger();
-        LargeInteger current = new LargeInteger(this);
-        LargeInteger multi = new LargeInteger(multiplier);
-        while (current.value.size() >= 1) {
-            if (current.value.getLast() != 0) {
-                result = result.add(multi);
-            }
-            multi = multi.shiftLeft();
-            current = current.shiftRight();
+        if (this.size == 1) {
+            return new LargeInteger(this.value[head] & multiplier.value[head]);
         }
+        LargeInteger[] ALR = this.split();
+        LargeInteger[] BLR = multiplier.split();
+        LargeInteger d1 = ALR[0].multiply(BLR[0]);
+        LargeInteger d0 = ALR[1].multiply(BLR[1]);
+        LargeInteger d01 = (ALR[0].add(ALR[1])).multiply((BLR[0].add(BLR[1])));
+        return d1.shiftLeftbyn(this.size - 1).add((d01.subtract(d0).subtract(d1))).shiftLeftbyn((this.size - 1) / 2).add(d0);
+    }
+
+    public LargeInteger[] split() {
+        LargeInteger[] result = new LargeInteger[2];
+        result[0] = new LargeInteger(this);
+        result[0].tail = tail / 2;
+        result[0].size -= tail - tail / 2;
+        result[1] = new LargeInteger(this);
+        result[1].head = tail / 2;
+        result[1].size = this.size - result[0].size;
         return result;
     }
 
     public LargeInteger shiftLeft() {
-        LinkedList<Byte> newList = new LinkedList<>(value);
-        newList.offerLast((byte) 0);
-        return new LargeInteger(newList);
+        LargeInteger result = new LargeInteger(this);
+        result.insertAtLast((byte) 0);
+        return result;
+    }
+
+
+    public LargeInteger shiftLeftbyn(int n) {
+        LargeInteger result = new LargeInteger(this);
+        while (n > 0) {
+            n--;
+            result.insertAtLast((byte) 0);
+        }
+        return result;
     }
 
     public LargeInteger subtract(LargeInteger toSubtract) {
-        LinkedList<Byte> diff = new LinkedList<>();
-        LinkedList<Byte> l = new LinkedList<>(this.value);
-        LinkedList<Byte> s = new LinkedList<>(toSubtract.value);
-        byte cout = 0;
-        while (s.size() > 0) {
-            int a = l.removeLast();
-            int b = s.removeLast();
-            int res = 0;
-            if (a == 0) {
-                if (b + cout >= 1) {
-                    res = 2 - b - cout;
-                    cout = 1;
-                }
-            } else {
-                if (b + cout >= 2) {
-                    cout = 1;
-                    res = 1;
-                } else {
-                    res = a - b - cout;
-                    cout = 0;
-                }
-            }
-            diff.offerFirst((byte) res);
+        LargeInteger diff = new LargeInteger();
+        LargeInteger l = new LargeInteger(this);
+        LargeInteger s = new LargeInteger(toSubtract);
+        //ASSUME L>S
+        while (s.size < l.size) {
+            s.insertAtHead((byte) 0);
         }
-        while (l.size() > 0) {
-            int a = l.removeLast();
-            if (cout == 1) {
-                if (a == 1) {
-                    cout = 0;
-                    a = 0;
-                } else {
-                    a = 1;
-                }
-            }
-            diff.offerFirst((byte) a);
+        byte bout = 0;
+        while (s.size > 0) {
+            byte a = l.removeLast();
+            byte b = s.removeLast();
+            byte d = (byte) (a ^ b ^ bout);
+            bout = (byte) ((a & bout) ^ (a & b) ^ (b & bout));
+            diff.insertAtHead(d);
         }
         while (diff.peekFirst() == 0) {
             diff.removeFirst();
@@ -120,36 +170,42 @@ public class LargeInteger implements Comparable<LargeInteger> {
     }
 
 
+    public byte removeFirst() {
+        byte res = value[head];
+        head += 1;
+        size -= 1;
+        if (head >= value.length) {
+            head = 0;
+        }
+        if (size <= 0) {
+            System.out.println("ERROR: INVALID REMOVE");
+            System.exit(-1);
+        }
+        return res;
+    }
+
+    private byte peekFirst() {
+        return value[head];
+    }
+
+
     public LargeInteger add(LargeInteger toAdd) {
-        if (toAdd.value.size() > this.value.size()) {
+        if (toAdd.size > this.size) {
             return toAdd.add(this);
         }
-        LinkedList<Byte> sum = new LinkedList<>();
-        LinkedList<Byte> l = new LinkedList<>(this.value);
-        LinkedList<Byte> s = new LinkedList<>(toAdd.value);
+        LargeInteger sum = new LargeInteger();
+        LargeInteger l = new LargeInteger(this);
+        LargeInteger s = new LargeInteger(toAdd);
+        while (s.size < l.size) {
+            s.insertAtHead((byte) 0);
+        }
         byte cout = 0;
-        while (s.size() > 0) {
-            int a = l.removeLast() + s.removeLast() + cout;
-            if (a >= 2) {
-                cout = 1;
-                a -= 2;
-            } else {
-                cout = 0;
-            }
-            sum.offerFirst((byte) a);
-        }
-        while (l.size() > 0) {
-            int a = l.removeLast() + cout;
-            if (a >= 2) {
-                cout = 1;
-                a -= 2;
-            } else {
-                cout = 0;
-            }
-            sum.offerFirst((byte) a);
-        }
-        if (cout != 0) {
-            sum.offerFirst((byte) 1);
+        while (s.size > 0) {
+            byte a = l.removeLast();
+            byte b = s.removeLast();
+            byte su = (byte) (a ^ b ^ cout);
+            cout = (byte) ((a & b) ^ (cout & (a ^ b)));
+            sum.insertAtHead(su);
         }
         LargeInteger res = new LargeInteger(sum);
         while (res.compareTo(Group.N) > 0) {
@@ -172,22 +228,27 @@ public class LargeInteger implements Comparable<LargeInteger> {
 
     @Override
     public int compareTo(LargeInteger o) {
-        if (o.value.size() > this.value.size()) {
+        if (o.size > this.size) {
             return -1;
-        } else if (o.value.size() < this.value.size()) {
+        } else if (o.size < this.size) {
             return 1;
         }
         int result = 0;
-        boolean found = false;
-        for (int i = 0; i < this.value.size(); i++) {
-            byte a = this.value.removeFirst();
-            byte b = o.value.removeFirst();
-            if (!found && a != b) {
-                found = true;
-                result = a - b;
+        for (int i = 0; i < this.size; i++) {
+            int aindex = head + i;
+            if (aindex >= value.length) {
+                aindex = 0;
             }
-            this.value.offerLast(a);
-            o.value.offerLast(b);
+            int bindex = o.head + i;
+            if (bindex >= value.length) {
+                bindex = 0;
+            }
+            byte a = this.value[aindex];
+            byte b = o.value[bindex];
+            if (a != b) {
+                result = a - b;
+                break;
+            }
         }
         return result;
     }
