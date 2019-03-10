270a323441a196a7d333281a31fb2fa14057849f
Hao Yang <yanghao.anderson@gmail.com>
2019-03-10 05:38:28 -0400

    Implemented karatsuba. Still, the threshold is high and wont be used.

--- Group-Signature-Schemes/src/One_way_accumulator/LargeInteger.java
+++ Group-Signature-Schemes/src/One_way_accumulator/LargeInteger.java
@@ -3,26 +3,60 @@ package One_way_accumulator;
 public class LargeInteger {
 
 
-    public int[] mag = new int[200];
+    public int[] mag;
     public int size;
+    public final int default_arr_size = 200;
+    public final int KA_threshold = 5;
 
     public LargeInteger() {
+        mag = new int[default_arr_size];
         mag[0] = 0;
-        size = 0;
+        size = 1;
     }
 
     public LargeInteger(int[] mag, int size) {
         this.mag = mag;
         this.size = size;
+        removeLeadingZeros();
     }
 
+    //Accepts hex string
     public LargeInteger(String val) {
+        mag = new int[default_arr_size];
+        int size = val.length() / 4 + 1;
+        for (int i = size; i >= 1; i--) {
+            int stringpos = val.length() - i * 4;
+            String substring = val.substring(Math.max(0, stringpos), stringpos + 4);
+            if (substring.equals("")) {
+                mag[size - i] = 0;
+            } else {
+                mag[size - i] = Integer.parseUnsignedInt(substring, 16);
+            }
+        }
+        this.mag = removeLeadingZeros(mag);
+        for (size = 0; size < this.mag.length && this.mag[size] != 0; size++)
+            ;
+        this.size = size;
+    }
 
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < size; i++) {
+            StringBuilder val = new StringBuilder(Integer.toHexString(mag[i]));
+            while (val.length() < 4) {
+                val.insert(0, "0");
+            }
+            sb.append(val);
+            sb.append(" ");
+        }
+        return sb.toString().toUpperCase();
     }
 
     public LargeInteger(int val) {
         int lo = val & 0xffff;
         int hi = val >> 16;
+        mag = new int[default_arr_size];
         if (hi == 0) {
             mag[0] = lo;
             size = 1;
@@ -38,7 +72,7 @@ public class LargeInteger {
         if (val.size > this.size) {
             return val.add(this);
         }
-        int[] resultMag = new int[200];
+        int[] resultMag = new int[default_arr_size];
         int result_size = this.size + 1;
         int cout = 0;
         for (int i = 0; i < result_size; i++) {
@@ -63,36 +97,138 @@ public class LargeInteger {
         return new LargeInteger(resultMag, result_size);
     }
 
+    //The input is always going to be smaller than this value in the scheme
+    public LargeInteger subtract(LargeInteger val) {
+        //If the size of val is larger than this large integer, swap them
+        int[] resultMag = new int[default_arr_size];
+        int result_size = this.size + 1;
+        int cout = 0;
+        for (int i = 0; i < result_size; i++) {
+            int a = 0;
+            int b = 0;
+            if (i < val.size) {
+                int val_index = val.size - i - 1;
+                b = val.mag[val_index];
+            }
+            if (i < this.size) {
+                int this_index = this.size - i - 1;
+                a = this.mag[this_index];
+            }
+            int res_index = result_size - i - 1;
+            long temp = 0;
+            temp += a;
+            temp -= b;
+            temp += cout;
+            cout = 0;
+            while (temp < 0) {
+                temp += 65536;
+                cout -= 1;
+            }
+
+            resultMag[res_index] = (int) (temp & 0xffff);
+        }
+        return new LargeInteger(resultMag, result_size);
+    }
+
     //Naive Multiplication
     public LargeInteger naive_multiply(LargeInteger val) {
         //If the size of val is smaller than this large integer, swap them
-        if (val.size < this.size) {
-            return val.naive_multiply(this);
-        }
-        int[] resultMag = new int[200];
+//        if (val.size < this.size) {
+//            return val.naive_multiply(this);
+//        }
+        int[] resultMag = new int[default_arr_size];
         int result_size = this.size + val.size + 1;
         for (int i = 0; i < this.size; i++) {
-            int cout = 0;
+            int this_index = this.size - i - 1;
+            long a = this.mag[this_index];
+            long cout = 0;
             for (int j = 0; j < val.size; j++) {
                 int val_index = val.size - j - 1;
-                int b = val.mag[val_index];
-                int this_index = this.size - i - 1;
-                int a = this.mag[this_index];
+                long b = val.mag[val_index];
                 int res_index = result_size - i - j - 1;
                 long temp = 0;
-//                System.out.println(a + " " + b + " " + cout);
-//                System.out.println(this_index + " " + val_index + " " + res_index);
                 temp += a * b;
                 temp += cout;
                 temp += resultMag[res_index];
-                cout = (int) (temp >> 16);
+                cout = temp >> 16;
                 resultMag[res_index] = (int) (temp & 0xffff);
             }
-
-            //This should not cause overflow since we are computing from lowest significant bit.
+            //This should not cause out of bound since we are computing from lowest significant bit.
             resultMag[result_size - i - val.size - 1] += cout;
         }
         return new LargeInteger(resultMag, result_size);
+    }
+
+
+    public LargeInteger KA_multiply(LargeInteger val) {
+        if (val.size < KA_threshold || this.size < KA_threshold) {
+            return naive_multiply(val);
+        }
+        int min_size = Math.min(val.size, this.size);
+        int middle = min_size / 2;
+        LargeInteger hi_val = new LargeInteger(arrayCopy(val.mag, 0, middle), middle);
+        LargeInteger lo_val = new LargeInteger(arrayCopy(val.mag, middle, val.size), val.size - middle);
+        LargeInteger hi_this = new LargeInteger(arrayCopy(mag, 0, middle), middle);
+        LargeInteger lo_this = new LargeInteger(arrayCopy(mag, middle, size), size - middle);
+
+//        System.out.println("hival: " + hi_val);
+//        System.out.println("loval: " + lo_val);
+//        System.out.println("hithi: " + hi_this);
+//        System.out.println("lothi: " + lo_this);
+
+
+        LargeInteger z0 = lo_val.KA_multiply(lo_this);
+        LargeInteger z1 = (lo_val.add(hi_val)).KA_multiply((lo_this.add(hi_this)));
+        LargeInteger z2 = hi_val.KA_multiply(hi_this);
+        LargeInteger temp = (z1.subtract(z2)).subtract(z0);
+        temp.size += (middle + 1);
+        z2.size += 2 * (middle + 1);
+
+//        System.out.println("z0 :" + z0);
+//        System.out.println("z1 :" + z1);
+//        System.out.println("z2 :" + z2);
+//        System.out.println("temp :" + temp);
+//        System.out.println("z2+temp: " + z2.add(temp));
+        return z2.add(temp).add(z0);
+    }
+
+    //Remove the leading zeros from input array
+    public int[] removeLeadingZeros(int[] mag) {
+        int keep, pos = 0;
+        int[] res = new int[default_arr_size];
+        for (keep = 0; keep < mag.length && mag[keep] == 0; keep++)
+            ;
+        for (; keep < mag.length && mag[keep] != 0; keep++) {
+            res[pos] = mag[keep];
+            pos++;
+        }
+        return res;
+    }
+
+    //Remove the leading zeros from current LargeInteger
+    public void removeLeadingZeros() {
+        int keep, pos = 0;
+        for (keep = 0; keep < size && mag[keep] == 0; keep++)
+            ;
+        if (keep == 0)
+            return;
+        int oldsize = size;
+        size -= keep;
+        for (; keep < oldsize; keep++) {
+            mag[pos] = mag[keep];
+            mag[keep] = 0;
+            pos++;
+        }
+    }
+
+    public int[] arrayCopy(int[] mag, int start, int end) {
+        int[] res = new int[default_arr_size];
+        int pos = 0;
+        for (int i = start; i < end; i++) {
+            res[pos] = mag[i];
+            pos++;
+        }
+        return res;
     }
 
 }
--- Group-Signature-Schemes/src/One_way_accumulator/Main.java
+++ Group-Signature-Schemes/src/One_way_accumulator/Main.java
@@ -5,23 +5,61 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        LargeInteger a = new LargeInteger(831028043);
-        LargeInteger b = new LargeInteger(489159823);
-        long t = System.nanoTime();
-        LargeInteger c = a.naive_multiply(b);
-        long newt = System.nanoTime();
-        System.out.println(newt - t);
-        BigInteger x = new BigInteger("831028043");
-        BigInteger y = new BigInteger("489159823");
+        BigInteger x = new BigInteger(100, new Random());
+        BigInteger y = new BigInteger(100, new Random());
+
+//        BigInteger x = new BigInteger("5b3678248", 16);
+//        BigInteger y = new BigInteger("2e42faed9", 16);
+
+        String xhex = x.toString(16);
+        String yhex = y.toString(16);
+
+        System.out.println("BI x: " + xhex);
+        System.out.println("BI y: " + yhex);
+
+        LargeInteger a = new LargeInteger(xhex);
+        LargeInteger b = new LargeInteger(yhex);
+        System.out.println("LI x: " + a);
+        System.out.println("LI y: " + b);
+        long t, newt;
+
         t = System.nanoTime();
         x.multiply(y);
         newt = System.nanoTime();
-        System.out.println(newt - t);
-        System.out.println("a[0]: " + a.mag[0]);
-        System.out.println("a[1]: " + a.mag[1]);
+        System.out.println("BI~100digit: " + (newt - t));
+
+        t = System.nanoTime();
+        a.naive_multiply(b);
+        newt = System.nanoTime();
+        System.out.println("LI~100digit: " + (newt - t));
+
+//        t = System.nanoTime();
+//        a.KA_multiply(b);
+//        newt = System.nanoTime();
+//        System.out.println("LI~100digit KA: " + (newt - t));
+
+
+        System.out.println(x.multiply(y).toString(16));
+        System.out.println(a.naive_multiply(b).toString());
+//
+//        System.out.println(x.add(y).toString(16));
+//        System.out.println(a.add(b));
+//
+//
+//        System.out.println(x.subtract(y).toString(16));
+//        System.out.println(a.subtract(b));
+
+
+//        System.out.println(a.KA_multiply(b));
+
+//        System.out.println(a.KA_multiply(b));
+//        System.out.println(a.naive_multiply(b));
+
+//        System.out.println("a[0]: " + a.mag[0]);
+//        System.out.println("a[1]: " + a.mag[1]);
 //        System.out.println("a[2]: " + a.mag[2]);
-        for (int i = 0; i < c.size; i++) {
-            System.out.println("c[" + i + "]: " + c.mag[i]);
-        }
+//        for (int i = 0; i < c.size; i++) {
+//            System.out.println("c[" + i + "]: " + c.mag[i]);
+//        }
     }
 }
